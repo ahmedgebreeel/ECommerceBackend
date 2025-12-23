b00e52fa28105e3fc0f8704095d3e4398c6310a4
Ahmed Gebreel <ahmedgebreeeel@gmail.com>
2025-12-23 14:20:46 +0200

    No need for HierarchyPath just need parentId for categoryDto

--- ECommerce.Business/DTOs/Categories/CategoryDto.cs
+++ ECommerce.Business/DTOs/Categories/CategoryDto.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public string HierarchyPath { get; set; } = string.Empty;
+
+        public int? ParentId { get; set; }
     }
 }
